7294e16b24f94ea386991a1ef862f630843ed0f9
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-05-09 16:09:16 +0200

    finished Hero section

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -1,8 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import polygon from '../../../images/hero/Polygon.svg';
-import polygon2 from '../../../images/hero/Polygon2.svg';
+
 import insta from '../../../images/hero/Insta.svg';
 import facebook from '../../../images/hero/Facebook.svg';
 import linkedIn from '../../../images/hero/linkedIn.svg';
@@ -11,53 +10,16 @@ import phone2 from '../../../images/hero/Phone2.webp';
 
 
 const Hero = () => {
-    const [isLargeScreen, setIsLargeScreen] = useState(true);
-    console.log(isLargeScreen);
-
-    useEffect(() => {
-        const handleWindowResize = () => {
-            if (typeof window !== 'undefined' && window.innerWidth >= 841) {
-                setIsLargeScreen(true);
-            } else {
-                setIsLargeScreen(false);
-            }
-        };
-    
-        window.addEventListener("resize", handleWindowResize);
-    
-        return () => {
-            window.removeEventListener("resize", handleWindowResize);
-        };
-    }, []);
-
 
   return (
-    <section>
-        <div className="mx-auto xl:mt-[244px] mt-[50px] sm:px-[16px] md:grid md:px-[32px] xl:px-[120px]">
-        <h1 className='sm:w-[322px] text-stone-50 sm:text-[50px] text-[46px] font-bold sm:text-start text-center'>Welcome To Sister Street App</h1>
-            <div className='flex relative mt-[40px] md:absolute'>
-            {isLargeScreen ? (
-          <Image
-            className='absolute md:mt-[0px] max-w-none max-h-none left-1/2 transform -translate-x-1/2  md:w-[977px] md:h-[650px] md:left-[85px] md:top-[120px] top-[40px] md:-translate-x-0 z-1'
-           priority
-           src={polygon2}
-           height={430}
-           width={430}
-           alt="Polygon"
-           /> 
-          ) : (
-            <Image
-            className='absolute max-w-none max-h-none left-1/2 transform -translate-x-1/2 top-[40px] z-1'
-           priority
-           src={polygon}
-           height={430}
-           width={430}
-           alt="Polygon"
-           /> 
-          )}
+    <section 
+    className="bg-no-repeat relative bg-hero-pattern sm:bg-[left_-2px_top_240px] bg-[length:390px_429px] md:bg-[length:977px_650px] md:bg-[right_-270px_top_175px] xl:bg-[right_-96px_top_100px] xl:bg-[length:_977px]"
+  >
+        <div className="mx-auto xl:w-[1440px] xl:mt-[244px] mt-[50px] sm:px-[16px] md:px-[32px] md:flex flex-col xl:px-[120px]">
+        <h1 className='xl:leading-[120%] xl:text-[70px] xl:w-[451px] sm:w-[322px] text-stone-50 sm:text-[50px] text-[46px] font-bold sm:text-start text-center'>Welcome To Sister Street App</h1>
      
             <Image
-            className='sm:mt-[52px]  max-w-none max-h-none h-[300px] md:absolute md:left-[345px] md:w-[274px] md:h-[500px] md:top-[-76px] z-10'
+            className='sm:top-[255px] max-w-none max-h-none h-[300px] absolute md:right-[180px] md:w-[274px] md:h-[500px] md:top-[89px] z-10 xl:right-[446px]'
             priority
             src={phone1}
             height={303}
@@ -65,20 +27,20 @@ const Hero = () => {
             alt="Facebook"
             />
             <Image
-            className='sm:mt-[68px] ml-[25px] z-10 max-w-none max-h-none h-[330px] md:absolute md:top-[60px] md:w-[274px] md:h-[500px] md:left-[545px] '
+            className='sm:top-[283px] sm:left-[196px] z-10 max-w-none max-h-none h-[320px] absolute md:top-[259px] md:w-[274px] md:h-[500px] md:left-[670px] xl:left-[1049px]'
             priority
             src={phone2}
             height={20}
             width={170}
             alt="Facebook"
             />
-            </div>
+  
             
 
-            <p className='md:w-[377px] mt-[40px] md:mt-[24px] text-stone-50 md:text-[24px] text-[20px] font-normal text-start z-10'>Sister Street is a women-only app  that provides a secure walking experience, particularly at night. Users connect with a supportive community through text, call or walking together.</p>
+            <p className='xl:w-[476px] md:w-[377px] mt-[440px] md:mt-[24px] text-stone-50 md:text-[24px] text-[20px] font-normal text-start z-10'>Sister Street is a women-only app  that provides a secure walking experience, particularly at night. Users connect with a supportive community through text, call or walking together.</p>
 
-            <button className='rounded-[30px] mt-[56px] py-[12px] bg-pink-500 w-full text-neutral-900 text-[22px] font-extrabold z-10'>JOIN US</button>
-            <div className='flex justify-between mt-[56px] px-[37px]'>
+            <button className='rounded-[30px] mt-[56px] py-[12px] bg-pink-500 w-full text-neutral-900 text-[22px] font-extrabold z-10 md:w-[284px]'>JOIN US</button>
+            <div className='flex justify-between mt-[56px] px-[37px] md:w-[284px] md:px-[1px]'>
             <Image
             priority
             src={facebook}
@@ -101,8 +63,8 @@ const Hero = () => {
             alt="linkedIn"
             />
 
-            </div>
-        </div>
+      </div>
+    </div>
     </section>
   )
 }
